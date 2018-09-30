326424ab43768e3d5ce34ce55e263e7a12118004
Angela Mitchell <anglarenmit@gmail.com>
2018-09-30 17:09:59 -0500

    Draft of validations

--- main.py
+++ main.py
@@ -1,87 +1,80 @@
 from flask import Flask, request, render_template
 
-form = """
-
-<!doctype html>
-
-<html>
-
-    <body>
-
-        <h1>User Signup</h1>
-
-        <form method="post">
-
-            <label>Username:
-                <input name="username" type="text" />
-            </label>
-
-            <br>
-
-            <label>Password:
-                <input name="password" type="password" />
-            </label>
-
-            <br>
-
-            <label>Verify Password:
-                <input name="verify" type="password" />
-            </label>
-
-            <br>
-
-            <label>Email (optional):
-                <input name="email" type="text" />
-            </label>
-
-            <br>
-
-            <label>Beginner
-                <input type="radio" name="coding_level" value="bg" />
-            </label>
-
-            <label>Intermediate
-                <input type="radio" name="coding_level" value="in" />
-            </label>
-
-            <label>Advanced
-                <input type="radio" name="coding_level" value="ad" />
-            </label>
-
-            <br>
-
-            <label>Professional Development
-                <input type="checkbox" name="coding_goal" value="first-cb" />
-            </label>
-
-            <br>
-
-            <label>Personal Hobby
-                <input type="checkbox" name="coding_goal" value="second-cb" />
-            </label>
-
-            <br>
-
-            <input type="submit" />
-
-        </form>
-
-    </body>
-
-</html>
-
-"""
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return form
+    return render_template('home_page.html')
 
 @app.route("/", methods=['POST'])
 def welcome():
     username = request.form['username']
-    return '<h1>Welcome, ' + username + '</h1>'
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+    
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
 
+    if username == "":
+        username_error = 'Enter a username'
+        #return render_template('home_page.html', username=username, username_error=username_error)
+    
+    if " " in username:
+        username_error = "Username invalid"
+    
+    if len(username) < 3 or len(username) > 20:
+        username_error = "Username invalid"
+
+    if password == "":
+        password_error = 'Enter a password'
+        password = ""
+
+    if verify == "":
+        verify = 'Reenter your password'
+        verify = ""
+    
+    if password != verify:
+        if password == "":
+            password_error = 'Enter a password'
+            password = ""
+        elif verify == "":
+            verify_error = 'Reenter your password'
+            verify = ""
+        else:
+            password_error = 'Passwords do not match'
+            verify_error = 'Passwords do not match'
+            password = ""
+            verify = ""
+
+    if email != "":
+        email = email
+    
+    if " " in email:
+        email_error = 'Email address invalid'
+        #return render_template('home_page.html', email=email, email_error=email_error)
+    
+    if "@" not in email and "." not in email:
+        email_error = 'Email address invalid'
+        #return render_template('home_page.html', email=email, email_error=email_error)
+
+    if len(email) < 3 or len(email) < 20:
+        email_error = 'Email address invalid'
+        #return render_template('home_page.html', email=email, email_error=email_error)
+        
+    #TODO add validation for email that detects '@' and ".com, .org", etc.
+    
+    if not username_error and not password_error and not verify_error or not email_error:
+        return render_template('welcome.html', name=username)
+    
+    else:
+        return render_template('home_page.html',
+        username_error=username_error, 
+        password_error=password_error,
+        verify_error=verify_error,
+        email_error=email_error)
+        
 app.run()
