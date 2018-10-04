23e41e81ad6ccfafbd12f7691121b006c3057f83
Angela Mitchell <anglarenmit@gmail.com>
2018-10-03 23:43:30 -0500

    Testing refactoring validation

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ app.config['DEBUG'] = True
 def index():
     return render_template('home_page.html')
 
+def no_spaces(string):
+    for char in string:
+        if char == " ":
+            return False
+    return True
+
+def is_empty(string):
+    if string == " " or string == "":
+        return True
+    return False
+
+def valid_len(string):
+    if len(string) < 3 or len(string) > 20:
+        return False
+    return True
+
+def valid_email(string):
+    p_count = 0
+    at_count = 0
+    for char in string:
+        if char == ".":
+            p_count += 1
+        elif char == "@":
+            at_count += 1
+    
+    # valid_len and no_spaces work; p_count and at_count don't work
+    if valid_len(string) == False or no_spaces(string) == False and (p_count > 1 or at_count > 1):
+        return False
+    return True
+
 @app.route("/", methods=['POST'])
 def welcome():
     username = request.form['username']
@@ -19,22 +49,22 @@ def welcome():
     verify_error = ""
     email_error = ""
     
-    if " " in username or len(username) < 3 or len(username) > 20:
+    if no_spaces(username) == False or is_empty(username) == True or valid_len(username) == False:
         username_error = "Enter a valid username"
 
-    if password == "":
-        password_error = 'Enter a password'
+    if no_spaces(password) == False or is_empty(password) == True or valid_len(password) == False:
+        password_error = 'Enter a valid password'
         password = ""
 
-    if verify == "":
+    if no_spaces(verify) == False or is_empty(verify) == True or valid_len(verify) == False:
         verify = 'Reenter your password'
         verify = ""
     
     if password != verify:
-        if password == "":
+        if no_spaces(password) == False or is_empty(password) == True or valid_len(password) == False:
             password_error = 'Enter a password'
             password = ""
-        elif verify == "":
+        elif no_spaces(verify) == False or is_empty(verify) == True or valid_len(verify) == False:
             verify_error = 'Reenter your password'
             verify = ""
         else:
@@ -44,7 +74,7 @@ def welcome():
             verify = ""
 
     if len(email) > 1:
-        if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 20:
+        if valid_email(email) == False:
             email_error = 'Email address invalid'
 
     if not username_error and not password_error and not verify_error and not email_error:
